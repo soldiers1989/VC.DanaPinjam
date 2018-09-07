d5519bd4515bea8d5bfc9f3fab9ba147a884bc83
F1 <f1@hixiu.com>
2018-09-07 13:55:13 +0800

    add logs

--- IF.AdminSystem/Controllers/UserController.cs
+++ IF.AdminSystem/Controllers/UserController.cs
@@ -916,7 +916,7 @@ namespace NF.AdminSystem.Controllers
                 ret.result = result.result;
                 if (result.result != Result.SUCCESS)
                 {
-                    Log.WriteDebugLog("UserController::EditUserPhotos", "保存失败，清除用户缓存。");
+                    Log.WriteDebugLog("UserController::EditUserPhotos", "保存失败，原因：{0}", result.message);
                     ret.message = result.message;
                 }
                 else
