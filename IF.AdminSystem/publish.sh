b65dd83fe6b33c6af0d4ed3d1edf2cbaa39c4140
F1 <wx_f557820b7b574033bdfd87a59ce22c4c@git.code.tencent.com>
2018-10-23 22:01:49 +0800

    增加版本备份

--- IF.AdminSystem/publish.sh
+++ IF.AdminSystem/publish.sh
@@ -1,28 +1,60 @@
 #!/bin/bash
-echo "open project dir."
-cd /data/git/api.danapinjam.com/VC.DanaPinjam/
 
-echo "get master version."
-echo "1.git checkout master."
-
-git checkout master
-
-echo "2.git pull."
+cd /data/git/VC.DanaPinjam/
+git checkout .
 git pull
+cd /data/git/VC.DanaPinjam/IF.AdminSystem/
 
-echo "3.git checkout api.danapinjam.com"
-git checkout api.danapinjam.com
+echo "删除发布目录"
+sudo rm -rf /data/www/api.smalldebit.club/release/
 
-echo "4.git merge master"
-git merge master
+sudo mkdir /data/www/api.smalldebit.club/release -p
 
-git pull
+sudo dotnet publish -o /data/www/api.smalldebit.club/release -c release
 
-cd ./IF.AdminSystem/
+current_date=`date -d "-1 day" "+%Y%m%d%H%M"`
 
-echo "publish project."
-/usr/share/dotnet/dotnet publish
+echo "bak path is:$current_date"
 
-echo "run test."
-nohup /usr/share/dotnet/dotnet /data/git/api.danapinjam.com/VC.DanaPinjam/IF.AdminSystem/bin/Debug/netcoreapp2.1/publish/IF.AdminSystem.dll &
+echo "删除15天前的日志文件"
+sudo find /data/www/api.smalldebit.club/logs/ -mtime +15 -name "*.log" -exec rm -rf {} \;
 
+echo "清空nohup.out文件"
+sudo echo /dev/null > /data/www/api.smalldebit.club/nohup.out
+echo "开始备份"
+sudo echo "做为版本记录:$current_date" > /data/www/api.smalldebit.club/version.out
+sudo zip -r /data/www/$current_date.zip /data/www/api.smalldebit.club/*  > /dev/null 2>&1
+
+echo "备份完成"
+id=`sudo ps -aux |grep "dotnet"| awk '{if($12 == "'/data/www/api.smalldebit.club/IF.AdminSystem.dll'") {print $2}}'`
+if [ "$id" == "" ]; then
+        echo "dotnet IF.AdminSystem.dll is not running..."
+else
+        kill -9 $id
+        echo "already kill IF.AdminSystem.dll."
+fi
+echo "将发布后的文件拷贝到着站点"
+sudo \cp -r /data/www/api.smalldebit.club/release/* /data/www/api.smalldebit.club/
+
+cd /data/www/api.smalldebit.club
+
+sudo rm -rf /data/www/api.smalldebit.club/version.out
+echo "还原配置文件"
+
+sudo \cp appsettings.json.bak appsettings.json
+sudo \cp dbinit.ini.bak dbinit.ini
+
+echo "准备启动"
+sudo nohup /usr/share/dotnet/dotnet /data/www/api.smalldebit.club/IF.AdminSystem.dll &
+
+echo "成功"
+ps -aux|grep dotnet
+tailf nohup.out
+
+
+#zip -r /data/www/test.zip /data/www/api.smalldebit.club/*
+
+
+#unzip -o -d / /data/www/test.zip
+
+#find /data/www/api.smalldebit.club/ -mtime +15 -name "*.log" -exec rm -rf {} \;
